bdf29a92d579379b60c1c8d573d8e829713bc98b
enes yıldırım <44911153+entwty@users.noreply.github.com>
2021-01-30 13:53:04 +0300

    TPSCAM

---
+++ Assets/Game/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+   private Vector3 velocity;
+
+   public float smoothTimeZ;
+
+   public float smoothTimeX;
+
+   public float offsetZ = -20f;
+
+   public GameObject player;
+
+   private Vector3 originPos;
+
+   private void Start()
+   {
+       this.originPos = transform.position;
+   }
+
+   private void FixedUpdate()
+   {
+       float posX = Mathf.SmoothDamp(transform.position.x,this.player.transform.position.x,ref this.velocity.x,this.smoothTimeX);
+
+       float posZ = Mathf.SmoothDamp(transform.position.z, this.player.transform.position.z + this.offsetZ, ref this.velocity.z, this.smoothTimeZ);
+
+       transform.position = new Vector3(posX, transform.position.y, posZ);
+   }
+}
---
+++ Assets/Game/Scripts/TPSMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            float targetAngel = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + this.cam.eulerAngles.y;
+
+            float angel = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngel,ref this.turnSmoothVelocity,turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angel, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngel, 0f) * Vector3.forward;
+
+            this.characterController.Move(moveDir.normalized * this.speed * Time.deltaTime);
+
+
+        }
+    }
+}
